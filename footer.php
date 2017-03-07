cd72edffc948e5ea991c77c705fb0dc1d15e7884
Borhan Chowdhury <borhan.chittagong@gmail.com>
2017-03-07 16:11:24 +0600

    citetion

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
 <div id="comments">
-  
+  copyright <a target="_blank" href="https://badhan.org">@BADHAN</a> | 
+  developer <a target="_blank" href="https://facebook.com/borhan.chittagong">@borhan</a> |
+  frame design <a target="_blank" href="https://www.facebook.com/rai106">@raihan</a>
 </div>
