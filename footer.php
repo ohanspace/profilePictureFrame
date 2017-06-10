ed515fc88c0ecdca5f0e994b1450d23b5f1dfab4
Borhan Chowdhury <borhan.chittagong@gmail.com>
2017-06-10 22:13:54 +0600

    add frame 1 for world blood donors day

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <div id="comments">
   copyright <a target="_blank" href="https://badhan.org">@BADHAN</a> | 
   developer <a target="_blank" href="https://facebook.com/borhan.chittagong">@borhan</a> |
-  frame design <a target="_blank" href="https://www.facebook.com/rai106">@raihan</a>
+  frame design 1 <a target="_blank" href="https://www.facebook.com/rai106">@raihan</a>
+  frame design 2 <a target="_blank" href="https://www.facebook.com/xaeemm">@zaeem</a>
 </div>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml">
   <head>
-    <title>Support BADHAN Web App</title>
+    <title>Support BADHAN</title>
     <link rel="shortcut icon" href="https://badhan.org/img/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="css/croppie.css" rel="stylesheet" async="async" />
@@ -14,13 +14,22 @@
   <body>
     <div id="wrapper">
       <div id="content">
-        <h1><a href="https://badhan.org"><img src="images/badhan-bangla.png"></a></h1>
+        <h1 >
+          <a href="https://badhan.org"><img src="images/badhan-bangla.png"></a>
+        </h1>
+        <h2>
+          <a href="https://badhan.org" style="color:red">
+          Visit Main website - Register to SAVE LIFE 
+          </a>
+        </h2>
         <div id="preview">
           <div id="crop-area">
             <img src="images/default-image.jpg" id="profile-pic" />
           </div>
-          <img src="frames/frame-0.png" id="fg" data-design="0" />
+          <img src="frames/frame-1.png" id="fg" data-design="1" />
+          <p>(please drag your photo to fit in the correct position in the frame)</p>
         </div>
+
         <p>
           <button id="fb-set-pic" disabled>Set As <b>Facebook</b> Profile Picture</button>
           <button id="download" disabled>Download Profile Picture</button>
@@ -34,8 +43,8 @@
         <h2>Design</h2>
         <div id="designs">
           <img class="design active" src="frames/frame-0.png" data-design="0" />
-          <!-- <img class="design" src="frames/frame-1.png" data-design="1" />
-          <img class="design" src="frames/frame-2.png" data-design="2" /> -->
+          <img class="design" src="frames/frame-1.png" data-design="1" />
+          <!-- <img class="design" src="frames/frame-2.png" data-design="2" /> -->
         </div>
         <?php
         require_once __DIR__ . "/footer.php";
